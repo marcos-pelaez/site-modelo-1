8705558bc8ccfcb328bd621a2da857ef7b444a27
marcos-pelaez <marcos.smarketing@gmail.com>
2021-06-16 18:08:56 -0300

    Conclusão menu mobile

---
+++ be-js/be-script.js
@@ -0,0 +1,10 @@
+
+//sow and hide menu
+function menuMobile(){
+    var mostraMenu = document.getElementById('main-menu');
+    if(mostraMenu.style.display == 'block'){
+        mostraMenu.style.display = 'none';
+    }else{
+        mostraMenu.style.display = 'block'
+    }
+}
