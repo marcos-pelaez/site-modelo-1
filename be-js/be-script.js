d414f708dc12d40d120527931ebe28514b6fc9a3
marcos-pelaez <marcos.smarketing@gmail.com>
2021-06-21 12:58:05 -0300

    ajuste javascript

--- be-js/be-script.js
+++ be-js/be-script.js
@@ -2,9 +2,9 @@
 //sow and hide menu
 function menuMobile(){
     var mostraMenu = document.getElementById('main-menu');
-    if(mostraMenu.style.display == 'block'){
-        mostraMenu.style.display = 'none';
+    if(mostraMenu.style.display == 'none'){
+        mostraMenu.style.display = 'block';
     }else{
-        mostraMenu.style.display = 'block'
+        mostraMenu.style.display = 'none'
     }
 }
